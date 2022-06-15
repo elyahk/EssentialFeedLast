fd6ebfc6fbc3e4d96884e3773d189b2fd273dc3a
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-06-15 11:24:36 +0500

    Create  model

---
+++ EssentialFeedLast/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeedLast
+//
+//  Created by Eldorbek on 15/06/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
