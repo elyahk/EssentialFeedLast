db90643c85abd7efc2351dd8922de46a1b7deb90
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-07-11 11:30:48 +0500

    Improve naming and move decodable logic to FeedItemsMapper

--- EssentialFeedLast/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeedLast/Feed Api/RemoteFeedLoader.swift
@@ -31,7 +31,7 @@ public final class RemoteFeedLoader {
             switch result {
             case let .success(data, response):
                 do {
-                    let items = try FeedItemMapper.map(data: data, response: response)
+                    let items = try FeedItemaMapper.map(data: data, response: response)
                     completion(.success(items))
                 } catch {
                     completion(.failure(.invalidData))
@@ -43,26 +43,26 @@ public final class RemoteFeedLoader {
     }
 }
 
-private class FeedItemMapper {
+private class FeedItemaMapper {
     static func map(data: Data, response: HTTPURLResponse) throws -> [FeedItem] {
         guard response.statusCode == 200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
         return try JSONDecoder().decode(Root.self, from: data).items.map { $0.feedItem }
     }
-}
 
-private struct Root: Decodable {
-    let items: [Item]
-}
+    private struct Root: Decodable {
+        let items: [Item]
+    }
 
-private struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
 
-    var feedItem: FeedItem {
-        FeedItem(id: id, description: description, location: location, imageURL: image)
+        var feedItem: FeedItem {
+            FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
     }
 }
